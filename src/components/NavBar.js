63bb964291120b6e7507fafb357a04612d19bf44
Jose <jose_esteban_basualdo@hotmail.com>
2021-11-14 22:36:34 -0300

    Update NavBar.js
    cambia class por className ya que es obligatorio.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,28 +5,28 @@ function Navbar() {
   return (
     <div className="NavBar">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"> <img src = {logoTienda} alt ="logo" style={{"width": "100px", "height": "100px"}}/>  </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"> <img src = {logoTienda} alt ="logo" style={{"width": "100px", "height": "100px"}}/>  </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Productos</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Productos</a>
         </li>
         
-        <li class="nav-item">
-          <a class="nav-link ">Promociones</a>
+        <li className="nav-item">
+          <a className="nav-link ">Promociones</a>
         </li>
       </ul>
      
         
-        <button class="btn btn-outline-success" type="submit">Log-in</button>
+        <button className="btn btn-outline-success" type="submit">Log-in</button>
      
     </div>
   </div>

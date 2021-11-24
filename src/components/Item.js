7b06ed72f275cadd6048d934c3abc23ba74db7ea
Jose <jose_esteban_basualdo@hotmail.com>
2021-11-24 18:38:35 -0300

    agrega card
    Esta card de bootstrap es una prueba, en ella mostratemos los elementos obtenidos desde la API

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import logoTienda from '../logo3.png'
 
 function Item({post}) {
     return (
@@ -7,6 +8,21 @@ function Item({post}) {
         <h2> {post.id} </h2>
 
         <p>  {post.title}  </p>
+
+        
+        <div className ="card" style={{'width': '18rem'}} >
+
+        <div className="card-body" >
+        <img src={logoTienda} alt="logo prueba"                  /> 
+        <h5 className="card-title" > Card Title</h5>
+        <p className="card-text" > This is where we would display the product</p>
+        <a href="/#" className="btn btn-success" >Add to cart</a>
+
+        </div>
+
+        </div>
+
+        
         </div>
     )
 }

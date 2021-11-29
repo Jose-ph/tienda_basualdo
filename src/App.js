f3679f9264f733d385a5ebf73bb6f0c95166337f
Jose <jose_esteban_basualdo@hotmail.com>
2021-11-28 23:41:09 -0300

    Agrega primeras rutas
    Primeras rutas para probar el funcionamiento.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,45 @@ import ItemListContainer from './containers/ItemListContainer/ItemListContainer'
 
 import ItemDetailContainer from './containers/ItemDetailContainer/ItemDetailContainer';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
+
+     <Router>  
     <div className="App">
 
       <header className="App-header">
 
       < Navbar />
      
-      
       </header>
+
+      <Switch> 
+       
+        <Route exact path = "/">
 
        <ItemListContainer greeting="Productos de miel natural multifloral" />
 
-      {/* <ItemCount stock = "5" initial= "1"  />  */}
+       </Route>
+
+       <Route  path = "/category/:id">
+
+       <ItemListContainer greeting="Productos de miel natural multifloral" />
+
+       </Route>
+
+       <Route path ="/item/:id" >
 
       <ItemDetailContainer  />
-      
+
+      </Route>
+
+
+      </Switch>
     </div>
+    </Router>
   );
 }
 

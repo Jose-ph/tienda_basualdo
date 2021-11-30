e3f4ec3178d6996f4b12572e6f2805c155177759
Jose <jose_esteban_basualdo@hotmail.com>
2021-11-29 23:56:29 -0300

    agrega Navlink
    Cambia el componente Navbar, utilizando Navlink para la navegación. La home no muestra productos, porque no está configurado aun.

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,37 +1,41 @@
 
+import { useHistory } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import logoTienda from '../../logo3.png'
 import Cartwidget from '../Cartwidget/Cartwidget';
 
+
 function Navbar() {
+
+  let history= useHistory();
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="NavBar">
 
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/#"> <img src = {logoTienda} alt ="logo" style={{"width": "100px", "height": "100px"}}/>  </a>
+  <li className="nav-item"> <NavLink className="nav-link" to={'/'}  ><img src = {logoTienda} alt ="logo" style={{"width": "100px", "height": "100px"}}/></NavLink></li>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/#">Inicio</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/#">Electrónica</a>
-
-
-        </li>
-        
-        <li className="nav-item">
-          <a className="nav-link " href="/#">Joyas</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="/#">Ropa Hombres</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="/#">Ropa Mujer</a>
-        </li>
+       
+        <li className="nav-item"> <NavLink className="nav-link" to={'/'}  >Inicio</NavLink></li>
+        <li className="nav-item"> <NavLink className="nav-link" to={'/category/electronics'}  >Electrónica</NavLink></li>
+        <li className="nav-item"> <NavLink className="nav-link" to={'/category/jewelery'}  >Joyas</NavLink></li>
+        <li className="nav-item"> <NavLink className="nav-link" to={"/category/men's clothing"}  >Ropa Hombres </NavLink></li>
+        <li className="nav-item"> <NavLink className="nav-link" to={"/category/women's clothing"}  >Ropa Mujeres </NavLink></li>
       </ul>
 
       

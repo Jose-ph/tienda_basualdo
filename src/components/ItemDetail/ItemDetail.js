7f477e42700b73ab1e7cc1c757d22ffc0c6caf1b
Jose <jose_esteban_basualdo@hotmail.com>
2021-12-02 17:08:03 -0300

    agrega estilo a ItemDetail.js
    Se agregan estilos iniciales. para la visualización del detalle.

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -17,28 +17,28 @@ function ItemDetail({post}) {
         history.push('/')
     }
     return (
-        <div>
+        <div   >
         
-        <div>
+        <div className="d-flex justify-content-center"  >
 
-        <div className ="card" style={{'width': '18rem', 'backgroundColor':' #20b9ee '}} >
+        <div className ="card " style={{'width': '50rem', 'backgroundColor':' #20b9ee '}} >
 
-        <div className="card-body" >
-        <img src={post.image} alt="logo prueba"  style={ {'width': '100px'}}                  /> 
-        <h5 className="card-title" > {post.title} </h5>
-        <p className="card-text" > {post.price} </p>
-        <p className="card-text" > {post.description} </p>
+        <div className="card-body d-flex flex-column justify-content-around" >
+        <img className="align-self-center" src={post.image} alt="logo prueba"  style={ {'width': '100px'}}                  /> 
+        <h5 className="card-title align-self-center" > {post.title} </h5>
+        <p className="card-text align-self-center" > {post.price} </p>
+        <p className="card-text align-self-center" > {post.description} </p>
 
         <ItemCount stock= {stock} initial = {initial} />
         <button onClick={goBack} className="btn btn-success mt-2" >Volver</button>
        
 
-</div>
+        </div>
 
-</div>
+        </div>
 
 
-</div>
+        </div>
                 
         </div>
     )

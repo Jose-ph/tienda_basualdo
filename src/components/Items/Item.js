6ed725d36c2472aa1dabbdcf611f7b9e143a230a
Jose <jose_esteban_basualdo@hotmail.com>
2021-12-25 11:57:40 -0300

    Update Item.js

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -17,7 +17,7 @@ function Item({products}) {
         <div  className ="card" style={{'width': '18rem', 'backgroundColor':' #20b9ee '}} >
 
         <div className="card-body" >
-        <img src={products.image} alt="logo prueba"  style={ {'width': '100px'}}                  /> 
+        <img src={products.image} alt="logo prueba"  style={ {'width': '200px','height':'200px'}}                  /> 
         <h5 className="card-title" > {products.title} </h5>
         <p className="card-text" > {products.description} </p>
         <button onClick = {seeDetails} className="btn btn-success" >See details</button>
